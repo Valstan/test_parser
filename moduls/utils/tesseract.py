a54dfd1969fb680fe581495932020edc25e2ac8d
valstan <123qwemmm@mail.ru>
2021-06-25 16:38:39 +0300

    2

--- moduls/utils/tesseract.py
+++ moduls/utils/tesseract.py
@@ -4,11 +4,11 @@ import pytesseract
 # pytesseract.pytesseract.tesseract_cmd = 'c:\\tesseract_orc\\tesseract.exe'
 
 
-img = cv2.imread('dobr2.jpg')
+img = cv2.imread('111222.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 custom_config = r'--oem 3 --psm 6'
 print(pytesseract.image_to_string(img, lang='rus', config=custom_config))
 
-# cv2.imshow('Result', img)
-# cv2.waitKey(0)
+cv2.imshow('Result', img)
+cv2.waitKey(0)
