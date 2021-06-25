3a89382c007924e4d8a7c322f317deb6e3b36b30
valstan <123qwemmm@mail.ru>
2021-06-25 09:26:41 +0300

    изменил тесеракт для дебиан среды

--- moduls/utils/tesseract.py
+++ moduls/utils/tesseract.py
@@ -1,7 +1,7 @@
 import cv2
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = 'c:\\tesseract_orc\\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = 'c:\\tesseract_orc\\tesseract.exe'
 
 
 img = cv2.imread('dobr2.jpg')
@@ -9,6 +9,6 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 custom_config = r'--oem 3 --psm 6'
 print(pytesseract.image_to_string(img, lang='rus', config=custom_config))
-'''
-cv2.imshow('Result', img)
-cv2.waitKey(0)'''
+
+# cv2.imshow('Result', img)
+# cv2.waitKey(0)
